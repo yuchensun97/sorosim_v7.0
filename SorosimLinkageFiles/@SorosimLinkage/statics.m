659e9b411e675727ffcf4bdf5fc7319921e90a36
yuchensun97 <sunyuchen.fs@hotmail.com>
2024-03-15 15:27:27 +0800

    add error handler for u_rho

--- SorosimLinkageFiles/@SorosimLinkage/statics.m
+++ SorosimLinkageFiles/@SorosimLinkage/statics.m
@@ -1,6 +1,7 @@
 function q = statics(Tr, qu0, u_xi, u_rho)
 
     n_sact = Tr.n_sact;
+    n_ract = Tr.n_ract;
 
     if n_sact == 0
         u_xi = 0;
@@ -12,6 +13,19 @@ function q = statics(Tr, qu0, u_xi, u_rho)
         
         if any(u_xi >0)
             error('Cable can only apply negative force');
+        end
+    end
+
+    if n_ract == 0
+        u_rho = 0;
+    else
+        sz = size(u_rho);
+        if sz(1) ~= n_ract || sz(2)~=1
+            error('u_rho must match the size of radial actuators');
+        end
+
+        if any(u_rho > 0)
+            error('Radial actuator can only apply negative pressure');
         end
     end
     
