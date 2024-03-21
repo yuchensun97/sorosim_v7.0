1f960a4d12e567549dc307b5d9d9aa2296daed32
Yuchen Sun <sunyuchen.fs@hotmail.com>
2024-03-21 09:38:17 +0000

    trivial

--- tests/testNoDamp.m
+++ tests/testNoDamp.m
@@ -1,10 +1,9 @@
 clc;
 clear;
-rng(5);
 
 % initialization
 B_xi = [1 1 1 1 1 1;
-        0 0 0 0 0 0]';
+        2 2 2 1 0 0]';
 
 B_rho = [1 1];
 
@@ -12,6 +11,13 @@ L = createLinkage(B_xi, B_rho);
 ndof_xi = L.ndof_xi;
 ndof_rho = L.ndof_rho;
 [t, qqd] = L.dynamics();
+q_xi = qqd(:, 1:ndof_xi);
+q_rho = qqd(:, ndof_xi+1: ndof_rho+ndof_xi);
+figure;
+plot(t, q_rho(:,1));
+xlabel('time');
+ylabel('q rho main');
+exportgraphics(gcf, './figures/nodamp.pdf','ContentType','vector');
 
 function L = createLinkage(B_xi, B_rho)
     S = SorosimLink();
