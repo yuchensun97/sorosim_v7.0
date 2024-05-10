e7f700c20dff7b7cb16fde8ad5cbff5c20772649
yuchensun97 <sunyuchen.fs@hotmail.com>
2024-05-10 16:34:05 +0800

    fix propangation cable function

--- Custom/Reaching/cableFunc.m
+++ Custom/Reaching/cableFunc.m
@@ -1,8 +1,9 @@
-function ux = cableFunc(t, Xs, Fmax)
+function ux = cableFunc(t, Xs, Fmax, fend)
 % input:
 %   t  -- scalar, time
 %   Xs -- (nip, 1) vector, integration points
-%   Fmax -- scalr, maximum force applied to the cable
+%   Fmax -- scalar, maximum force applied to the cable
+%   fend -- scalar, force end at fend
 % returns:
 %   ux -- (nip, 1) vector, cable tension at integration points at t
 
@@ -16,7 +17,7 @@ function ux = cableFunc(t, Xs, Fmax)
     ux = zeros(nip, 1); % cable tension at integration points at t
 
     ux(Xs < pe) = 0;
-    ux(Xs >= pe & Xs < ps) = (1-ps)*Fmax*(Xs(Xs >= pe & Xs < ps)-pe)/(ps-pe);
-    ux(Xs >= ps & Xs < 1) = (1-Xs(Xs >= ps & Xs < 1))*Fmax;
-    ux(Xs >= 1) = 0;
+    ux(Xs >= pe & Xs < ps) = (1-ps/fend)*Fmax*(Xs(Xs >= pe & Xs < ps)-pe)/(ps-pe);
+    ux(Xs >= ps & Xs < fend) = (1-Xs(Xs >= ps & Xs < fend)/fend)*Fmax;
+    ux(Xs >= fend) = 0;
 end
