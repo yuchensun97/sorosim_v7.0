77d4b416d701c00e528342131066d58f7ecf10cf
yuchensun97 <sunyuchen.fs@hotmail.com>
2024-05-10 16:33:36 +0800

    trivial

--- testInput.m
+++
@@ -1,8 +0,0 @@
-function testInput()
-    p = inputParser;
-    defaultDamping = false;
-    addOptional(p, 'Damped', defaultDamping, @islogical);
-    parse(p, 'Damped', true);
-    disp('Damped is:');
-    disp(p.Results.Damped);
-end
